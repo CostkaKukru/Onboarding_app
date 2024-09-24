14dffe72a064eba50231d5c0ff6aa703764b26f2
CostkaKukru <fifqa87@gmail.com>
2024-09-24 12:03:00 +0200

    setting up Spring

--- src/test/java/com/example/demo/OnboardingAppApplicationTests.java
+++ src/test/java/com/example/demo/OnboardingAppApplicationTests.java
@@ -7,9 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class OnboardingAppApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(OnboardingAppApplication.class, args);
 	}
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
